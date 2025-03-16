688cd2d73973dda40215bc624896281450b72e8f
whyweexist <167321903+whyweexist@users.noreply.github.com>
2025-03-16 23:13:48 +0530

    Add files via upload

---
+++ internal/service/tix_service.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"github.com/whyweexist/evm/internal/domain"
+	"github.com/redis/go-redis/v9"
+	"gopkg.in/gomail.v2"
+	"sync"
+)
+
+type tixService struct {
+	mu                      sync.Mutex
+	redisCache              *redis.Client
+	googleServiceRepository domain.IGoogleServiceRepository
+	authRESTRepository      domain.IAuthRESTRepository
+	postgreSQLRepository    domain.IPostgreSQLRepository
+	mailer                  *gomail.Dialer
+}
+
+type TixOptions func(*tixService)
+
+func WithGoogleServiceRepository(
+	googleFormService domain.IGoogleServiceRepository,
+) TixOptions {
+	return func(service *tixService) {
+		service.googleServiceRepository = googleFormService
+	}
+}
+
+func WithRedisCache(
+	redisCache *redis.Client,
+) TixOptions {
+	return func(service *tixService) {
+		service.redisCache = redisCache
+	}
+}
+
+func WithAuthRESTRepository(
+	authRESTRepository domain.IAuthRESTRepository,
+) TixOptions {
+	return func(service *tixService) {
+		service.authRESTRepository = authRESTRepository
+	}
+}
+
+func WithPostgreSQLRepository(
+	postgreSQLRepository domain.IPostgreSQLRepository,
+) TixOptions {
+	return func(service *tixService) {
+		service.postgreSQLRepository = postgreSQLRepository
+	}
+}
+
+func WithMailer(mailer *gomail.Dialer) TixOptions {
+	return func(service *tixService) {
+		service.mailer = mailer
+	}
+}
+
+func NewTixService(
+	options ...TixOptions,
+) domain.ITixService {
+	service := &tixService{}
+	for _, option := range options {
+		option(service)
+	}
+	return service
+}
